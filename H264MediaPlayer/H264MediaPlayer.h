cbc61c2a77a31a632ad75abc8bacb2e91814eec7
Phung Nguyen <triphung@nxcomm.com>
2012-10-12 17:01:59 +0800

    Initial Commit

---
+++ H264MediaPlayer/H264MediaPlayer.h
@@ -0,0 +1,13 @@
+//
+//  H264MediaPlayer.h
+//  H264MediaPlayer
+//
+//  Created by NxComm on 10/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface H264MediaPlayer : NSObject
+
+@end
---
+++ H264MediaPlayer/H264MediaPlayer.m
@@ -0,0 +1,13 @@
+//
+//  H264MediaPlayer.m
+//  H264MediaPlayer
+//
+//  Created by NxComm on 10/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "H264MediaPlayer.h"
+
+@implementation H264MediaPlayer
+
+@end
